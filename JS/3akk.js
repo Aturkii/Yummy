87cca646c662ac57ff6af98b4a98ed3830a7e3d6
Ahmed Mahmoud Turki <100971093+Aturkii@users.noreply.github.com>
2024-01-28 22:06:15 +0200

    Update 3akk.js

--- JS/3akk.js
+++ JS/3akk.js
@@ -1,11 +1,1 @@
-for (let i = 0; i < arr.length; i++) {
-  cartoona += `
-    <div class="col-md-3">
-      <div onclick="getMealDetails('${arr[i].idMeal}')" class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
-        <img class="w-100" src="${arr[i].strMealThumb}" alt="" srcset="">
-        <div class="meal-layer position-absolute d-flex align-items-center text-black p-2">
-          <h3>${arr[i].strMeal}</h3>
-        </div>
-      </div>
-    </div>`;
-}
+
